6e620d4d51f51d5b04ac76c4a01c26fb0e808592
BlackAds <45212906+BlackAds@users.noreply.github.com>
2018-11-20 21:52:43 +0100

    Add files via upload

---
+++ add.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+	<form action="index.php" method="get">
+		<strong>Name</strong><br>
+		<input type="text" name="name"><br>
+		<br>
+		<strong>Begrüßung</strong><br>
+		<select name="gruss">
+			<option value="Guten Morgen">Guten Morgen</option>
+			<option value="Mahlzeit">Mahlzeit</option>
+			<option value="Guten Abend">Guten Abend</option>
+			<option value="Gute Nacht">Gute Nacht</option>
+		</select><br>
+		<br>
+		<input type="submit" value="Abschicken">
+	</form>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+	<?php
+	echo $_GET['gruss'].' '.$_GET['name'];
+	?>
+</body>
+</html>
