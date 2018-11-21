e0da07031d79de28b10cc5e5ad148a4f1f179890
BlackAds <45212906+BlackAds@users.noreply.github.com>
2018-11-21 11:54:50 +0100

    Add files via upload

---
+++ phrase_add.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Unbenanntes Dokument</title>
+</head>
+
+<body>
+	<form action="index.php" method="get">
+
+		<strong>I say Yes! to...</strong><br>
+		<select name="unfug-0">
+			<option value="Guten Morgen">Guten Morgen</option>
+			<option value="Mahlzeit">Mahlzeit</option>
+			<option value="Guten Abend">Guten Abend</option>
+			<option value="Gute Nacht">Gute Nacht</option>
+		</select>
+		<select name="unfug-1">
+			<option value="Guten Morgen">Guten Morgen</option>
+			<option value="Mahlzeit">Mahlzeit</option>
+			<option value="Guten Abend">Guten Abend</option>
+			<option value="Gute Nacht">Gute Nacht</option>
+		</select>
+		<select name="unfug-2">
+			<option value="Guten Morgen">Guten Morgen</option>
+			<option value="Mahlzeit">Mahlzeit</option>
+			<option value="Guten Abend">Guten Abend</option>
+			<option value="Gute Nacht">Gute Nacht</option>
+		</select><br>
+		<br>
+		<input type="submit" value="Abschicken">
+	</form>
+</body>
+</html>
